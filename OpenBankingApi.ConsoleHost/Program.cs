42a15df9079121134923c76d060715a3e61d886c
Roger Sillito <rogersillito@gmail.com>
2019-03-11 12:53:23 +0000

    add OWIN-based console host app for OB api

---
+++ OpenBankingApi.ConsoleHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace OpenBankingApi.ConsoleHost
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://127.0.0.1:12345"))
+            {
+                Console.ReadLine();
+            }
+            return 0;
+        }
+    }
+}
