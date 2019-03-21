de840db66332bb75e70e623afbddbee9c12cc791
Roger Sillito <rogersillito@gmail.com>
2019-03-21 15:44:11 +0000

    Add a AspNetCore project to test out Core controller generation.  Added separate NSwag, and a custom template for this: NB: this relies on NSWag.CodeGeneration.CSharp & NSWag.CodeGeneration including my new properties: https://github.com/RSuter/NSwag/pull/2033/files#diff-f14aab342b6f347552b53a9d54b41192R45

---
+++ OpenBankingApi_Core/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenBankingApi_core.Controllers;
+
+#region snippet_ApiControllerAttributeOnAssembly
+[assembly: ApiController]
+namespace OpenBankingApi_core
+{
+    public class Startup
+    {
+        #endregion snippet_ApiControllerAttributeOnAssembly
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IOpenBankingApiController, OpenBankingApiControllerImpl>();
+
+            #region snippet_ConfigureApiBehaviorOptions
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.SuppressConsumesConstraintForFormFileParameters = true;
+                    options.SuppressInferBindingSourcesForParameters = true;
+                    options.SuppressModelStateInvalidFilter = true;
+                    options.SuppressMapClientErrors = true;
+
+                    options.ClientErrorMapping[404].Link = 
+                        "https://httpstatuses.com/404";
+                });
+            #endregion
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
