a713cb2791338610badbe6633e3814583eafbf19
Roger Sillito <rogersillito@gmail.com>
2019-05-17 17:27:18 +0100

    Now uses Open Banking v3.1.1 (Generated code goes into version-named folders). Split out generated code - separate output for DTOs & controllers.  Client created without dtos so can share same reference. Remove OB 3.1.0 files.  Remove WebApi controller project.

---
+++ OpenBankingApi_Core/Services/OpenBankingProductsService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using OpenBankingApi.NSwagGenerated.v3_1_1;
+
+namespace OpenBankingApi.Services
+{
+    public class OpenBankingProductsService: IOpenBankingProductsImplementor
+    {
+        public Task<OBReadProduct2> ProductAsync(string accountId, string x_fapi_financial_id, string authorization,
+            string x_fapi_customer_last_logged_time = null, string x_fapi_customer_ip_address = null,
+            string x_fapi_interaction_id = null, string x_customer_user_agent = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<OBReadProduct2> ProductsAsync(string x_fapi_financial_id, string authorization, string x_fapi_customer_last_logged_time = null,
+            string x_fapi_customer_ip_address = null, string x_fapi_interaction_id = null, string x_customer_user_agent = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ regenerate_dtos.bat
@@ -0,0 +1,2 @@
+%~dp0ApiGenerator\bin\debug\ApiGenerator run %~dp0ApiGenerator\OpenBankingApi_v3.1.1_DTOs.nswag
+@ping 127.0.0.1 -n 3 > nul
