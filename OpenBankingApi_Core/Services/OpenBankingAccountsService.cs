58bfe66c770ce0dd51e64ad266e08a27c415b587
Roger Sillito <rogersillito@gmail.com>
2019-05-21 17:25:10 +0100

    Controller template generated with async/await. Regenerated controllers/dtos using NSwag 12.3.  Filled out the sample response object for /accounts so it serializes correctly..

--- OpenBankingApi_Core/Services/OpenBankingAccountsService.cs
+++ OpenBankingApi_Core/Services/OpenBankingAccountsService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using OpenBankingApi.NSwagGenerated.v3_1_1;
 
@@ -6,25 +7,37 @@ namespace OpenBankingApi.Services
 {
     public class OpenBankingAccountsService : IOpenBankingAccountsImplementor
     {
-        public Task<OBReadAccount3> AccountsGetAsync(string x_fapi_financial_id, string authorization, string x_fapi_customer_last_logged_time = null,
+        public async Task<OBReadAccount3> AccountsGetAsync(string x_fapi_financial_id, string authorization, string x_fapi_customer_last_logged_time = null,
             string x_fapi_customer_ip_address = null, string x_fapi_interaction_id = null, string x_customer_user_agent = null)
         {
-            return Task.FromResult(new OBReadAccount3
+            var accounts = new OBReadAccount3
             {
                 Data = new Data
                 {
                     Account = new List<OBAccount3>
+                    {
+                        new OBAccount3
                         {
-                            new OBAccount3
-                            {
-                                AccountId = "1234",
-                                Nickname = "Some Account"
-                            }
+                            AccountId = "1234",
+                            Currency = "GBP",
+                            Nickname = "Some Account"
                         }
+                    }
                 },
-                Links = new Links(),
-                Meta = new Meta()
-            });
+                Links = new Links
+                {
+                    Self = new Uri("http://wrong.com"),
+                    Prev = null,
+                    Next = null,
+                    First = null,
+                    Last = null
+                },
+                Meta = new Meta
+                {
+                    TotalPages = 1
+                }
+            };
+            return await Task.FromResult(accounts);
         }
         public Task<OBReadAccount3> AccountsGetAsync(string accountId, string x_fapi_financial_id, string authorization,
             string x_fapi_customer_last_logged_time = null, string x_fapi_customer_ip_address = null,
